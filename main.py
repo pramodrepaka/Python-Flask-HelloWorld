6a975830f8f04f1cb0dc749c17f0dc12339a9776
pramodrepaka <43452908+pramodrepaka@users.noreply.github.com>
2018-11-27 02:12:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hey its Python Flask application!'
+  return 'Welcome to Northwind!!!'
 
 if __name__ == '__main__':
   app.run()
